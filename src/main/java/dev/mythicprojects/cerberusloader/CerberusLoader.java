a96c0e05cfa098c386471ceb7b37496d8d4efdd9
Peridot <peridot491@protonmail.com>
2023-05-29 21:02:18 +0200

    Initial commit

---
+++ src/main/java/dev/mythicprojects/cerberusloader/CerberusLoader.java
@@ -0,0 +1,9 @@
+package dev.mythicprojects.cerberusloader;
+
+public final class CerberusLoader {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
